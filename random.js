0b009259e16d224e7e76d3e76919e4da1a8e2602
vicagbur <victorag@gmail.com>
2015-07-20 19:11:44 +0200

    segunda versión

--- random.js
+++ random.js
@@ -1,7 +1,9 @@
 var numero = Math.random();
 
+var str = 'MAYOR que 0,5';
+
 if (numero <= 0.5){
-  console.log('\n' + numero + ' MENOR que 0,5 \n');
-} else {
-  console.log('\n' + numero + ' MAYOR que 0,5 \n')
+  str = ' MENOR que 0,5');
 }
+
+console.log('\n' + numero + str + '\n');
